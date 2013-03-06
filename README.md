866fde21f5f13acf5a4bef9e561978bd352b7658
Tonin De Rosso Bolzan <admin@tonybolzan.com>
2013-03-06 10:54:06 -0300

    Added ajaxData feature

--- README.md
+++ README.md
@@ -1,32 +1,64 @@
 # Yii Other Extensions
 
-
 ## TWebLogRoute
 Alternative for CWebLogRoute with button to show the log
 
 ## TButtonColumn
-Represents a grid view column to extends CButtonColumn ajax functions.
-
+Represents a grid view column to extends CButtonColumn POST ajax functions.
 This extension function clones ajax "click" of delete button.
 ```php
 array(
-    'class'    => 'ext.TButtonColumn',
-    'header'   => 'Actions',
-    'template' => '{teste}',
-    'buttons'  => array(
-        'teste'=> array(
-            'url' => 'Yii::app()->controller->createUrl("controller/action",array("id"=>$data->primaryKey))',
-            'imageUrl'  => false,
-            'label'     => 'Teste',
-            'options'   => array(
+    'class' => 'ext.TButtonColumn',
+    'header' => 'Actions',
+    'template' =>'{teste}',
+    'buttons'=>array(
+        'teste'=>array(
+            'url'=>'Yii::app()->controller->createUrl("controller/action",array("id"=>$data->primaryKey))',
+            'imageUrl' => false,
+            'label' => 'Teste',
+            'options' => array(
                 'title' => 'Test',
+                'ajaxClick' => true,        // NEW
                 'ajaxConfirm' => 'Testing', // NEW
-                'ajaxClick'   => true,      // NEW
+                'ajaxData' => array(        // NEW
+                    'test1Post' => 'dataPostTest', // Raw data
+                    'test2Post' => 'js: $(th).data("test")', // JS function to get data
+                ),
             ),
         ),
     ),
 ),
 ```
-`ajaxConfirm` => String - Msg to confirm javascript or starting with `js:` make a javascript personalized
-
-`ajaxClick` => Boolean - True => Enable / False => Normal
+<table>
+    <thead>
+        <tr>
+            <td>Option</td>
+            <td>Type</td>
+            <td>Description</td>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>`ajaxClick`</td>
+            <td>Boolean</td>
+            <td>True => Enable (Ajax POST) / False => Normal button (GET)</td>
+        </tr>
+        <tr>
+            <td>`ajaxConfirm`</td>
+            <td>String</td>
+            <td>
+                The confirmation message to be displayed when button is clicked.
+                If not set or false, no confirmation message will be displayed.
+                If starting with `js:` make a javascript personalized (`this` refers to `<a>` tag).
+            </td>
+        </tr>
+        <tr>
+            <td>`ajaxData`</td>
+            <td>Array</td>
+            <td>
+                Aditional Data to send in POST request header (@see CJavaScript::encode())
+                (`this` refers to `<a>` tag).
+            </td>
+        </tr>
+    </tbody>
+</table>
--- TButtonColumn.php
+++ TButtonColumn.php
@@ -4,40 +4,53 @@
  * TButtonColumn class file.
  *
  * @author Tonin de Rosso Bolzan <admin@tonybolzan.com>
- * @copyright 2012, tonybolzan.com
+ * @copyright 2013, tonybolzan.com
  * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ * @version 1.1
  */
 Yii::import('zii.widgets.grid.CButtonColumn');
 
 /**
- * TButtonColumn represents a grid view column to extends CButtonColumn ajax functions.
- *
+ * TButtonColumn Represents a grid view column to extends CButtonColumn POST ajax functions.
  * This extension function clones ajax "click" of delete button.
  *
- *    array(
- *        'class' => 'ext.TButtonColumn',
- *        'header' => 'Actions',
- *        'template' =>'{teste}',
- *        'buttons'=>array(
- *            'teste'=>array(
- *                'url'=>'Yii::app()->controller->createUrl("controller/action",array("id"=>$data->primaryKey))',
- *                'imageUrl' => false,
- *                'label' => 'Teste',
- *                'options' => array(
- *                    'title' => 'Test',
- *                    'ajaxConfirm' => 'Testing', // NEW
- *                    'ajaxClick' => true,        // NEW
- *                ),
- *            ),
- *        ),
- *    ),
- * 
- * ajaxConfirm
- *     String - Msg to confirm javascript or starting with "js:" make a javascript personalized
+ * array(
+ *     'class' => 'ext.TButtonColumn',
+ *     'header' => 'Actions',
+ *     'template' =>'{teste}',
+ *     'buttons'=>array(
+ *         'teste'=>array(
+ *             'url'=>'Yii::app()->controller->createUrl("controller/action",array("id"=>$data->primaryKey))',
+ *             'imageUrl' => false,
+ *             'label' => 'Teste',
+ *             'options' => array(
+ *                 'title' => 'Test',
+ *                 'ajaxClick' => true,        // NEW v1.0
+ *                 'ajaxConfirm' => 'Testing', // NEW v1.0
+ *                 'ajaxData' => array(        // NEW v1.1
+ *                     'test1Post' => 'dataPostTest', // Raw data
+ *                     'test2Post' => 'js: $(th).data("test")', // JS function to get data
+ *                 ),
+ *             ),
+ *         ),
+ *     ),
+ * ),
  * 
  * ajaxClick
- *     Boolean - Enable/Normal
+ *  Boolean
+ *      True => Enable (Ajax POST) / False => Normal button (GET)
  * 
+ * ajaxConfirm
+ *  String
+ *      The confirmation message to be displayed when button is clicked.
+ *      If not set or false, no confirmation message will be displayed.
+ *      If starting with `js:` make a javascript personalized
+ *      `this` refers to <a> tag
+ * 
+ * ajaxData
+ *  Array
+ *      Aditional Data to send in POST request header @see CJavaScript::encode()
+ *      `this` refers to <a> tag
  */
 class TButtonColumn extends CButtonColumn {
 
@@ -47,19 +60,24 @@ class TButtonColumn extends CButtonColumn {
      */
     public function init() {
         
-        foreach ($this->buttons as $id => $button) {
+        foreach ($this->buttons as $id => &$button) {
             if (isset($button['options']['ajaxClick']) and $button['options']['ajaxClick']) {
                 
                 $confirmation = isset($button['options']['ajaxConfirm']) ? $button['options']['ajaxConfirm'] : false;
+                $data = (isset($button['options']['ajaxData']) and is_array($button['options']['ajaxData'])) ? $button['options']['ajaxData'] : array();
                 
-                $this->initAjaxClickButtons($id, $confirmation);
+                unset($button['options']['ajaxClick']);
+                unset($button['options']['ajaxConfirm']);
+                unset($button['options']['ajaxData']);
+                
+                $this->initAjaxClickButtons($id, $data, $confirmation);
             }
         }
 
         parent::init();
     }
 
-    protected function initAjaxClickButtons($button_id, $confirmation = false) {
+    protected function initAjaxClickButtons($button_id, $data = array(), $confirmation = false) {
         if (is_string($confirmation)) {
             $confirmation = "if(!confirm(" . CJavaScript::encode($confirmation) . ")) return false;";
         } else {
@@ -67,27 +85,28 @@ class TButtonColumn extends CButtonColumn {
         }
 
         if (Yii::app()->request->enableCsrfValidation) {
-            $csrfTokenName = Yii::app()->request->csrfTokenName;
-            $csrfToken = Yii::app()->request->csrfToken;
-            $csrf = "\n\t\tdata:{ '$csrfTokenName':'$csrfToken' },";
-        } else {
-            $csrf = '';
+            $data[Yii::app()->request->csrfTokenName] = Yii::app()->request->csrfToken;
         }
 
-        $this->buttons[$button_id]['click'] = <<<EOD
+        if($data) {
+            $jsData = ' data:' . CJavaScript::encode($data) . ', ';
+        } else {
+            $jsData = '';
+        }
+        
+        $this->buttons[$button_id]['click'] = <<<JS
 function() {
-	$confirmation
-	var th=this;
-	$.fn.yiiGridView.update('{$this->grid->id}', {
-		type:'POST',
-		url:$(this).attr('href'),$csrf
-		success:function(data) {
-			$.fn.yiiGridView.update('{$this->grid->id}');
-		}
-	});
-	return false;
+    $confirmation
+    $.fn.yiiGridView.update('{$this->grid->id}', {
+        type: 'POST',
+        url: $(this).attr('href'),$jsData
+        success: function(data) {
+            $.fn.yiiGridView.update('{$this->grid->id}');
+        }
+    });
+    return false;
 }
-EOD;
+JS;
     }
 
 }
